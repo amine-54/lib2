91f3a3b4ad1c20e0f0e8abe9e12f2d59948c201d
Mohamed Amine Manyani <mmanyani@e1r1p12.1337.ma>
2024-11-07 18:53:43 +0100

    frst bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanyani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 17:24:27 by mmanyani          #+#    #+#             */
+/*   Updated: 2024/11/07 18:53:17 by mmanyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
+
+int main()
+{
+	t_list *tst;
+	int	i;
+
+	i = 15;
+	tst = ft_lstnew(&i);
+	printf("%d", *(int *)(tst->content));
+}
